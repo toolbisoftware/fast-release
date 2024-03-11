c675679fd3a22de44cb55ce78c4f78b587bc216b
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-03-11 16:55:57 +0100

    Update config.rs

--- modules/fast-release/src/config.rs
+++ modules/fast-release/src/config.rs
@@ -2,6 +2,7 @@
 // Check the README file in the project root for more information.
 
 use crate::{
+  cli::{self, CliParams},
   constants::{CONFIG_FILE_EXTS, CONFIG_FILE_NAMES},
   error::{throw_error, FastReleaseError},
   git::branch::Branch,
@@ -29,7 +30,7 @@ enum ConfigBranchEnum {
 
 // TODO Add the settings inside of each of the declared modules for simplicity.
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct ConfigProject {
+pub struct ConfigProject {
   name: String,
   path: String,
   modules: Vec<String>,
@@ -38,6 +39,7 @@ struct ConfigProject {
 #[derive(Debug, Serialize, Deserialize)]
 struct RawConfig {
   version: u8,
+  tag_format: String,
   modules: Vec<String>,
   branches: Vec<ConfigBranchEnum>,
   project: Option<ConfigProject>,
@@ -46,19 +48,16 @@ struct RawConfig {
 
 #[derive(Debug, Clone)]
 pub struct Config {
-  version: u8,
-  modules: Vec<String>,
+  pub version: u8,
+  pub tag_format: String,
+  pub modules: Vec<String>,
   pub branches: Vec<Branch>,
-  project: Option<ConfigProject>,
-  projects: Option<Vec<ConfigProject>>,
+  pub project: Option<ConfigProject>,
+  pub projects: Option<Vec<ConfigProject>>,
+  pub dry_run: bool,
 }
 
-fn find_file() -> Result<PathBuf, FastReleaseError<'static>> {
-  debug!(
-    category = "CONFIG",
-    message = "Trying to find a configuration file."
-  );
-
+fn find_file(path_override: Option<String>) -> Result<PathBuf, FastReleaseError<'static>> {
   let path: PathBuf = std::env::current_exe().map_err(|error: Error| FastReleaseError {
     message: "Failed to get the executable's path.",
     category: Some("CONFIG"),
@@ -75,26 +74,49 @@ fn find_file() -> Result<PathBuf, FastReleaseError<'static>> {
     }
   };
 
-  for file_name in CONFIG_FILE_NAMES {
-    for file_ext in CONFIG_FILE_EXTS {
-      let file_name: String = format!("{}.{}", file_name, file_ext);
-      let path: PathBuf = path.join(&file_name);
+  if let Some(path_override) = path_override {
+    let path = path.join(&path_override);
 
-      if fs::metadata(&path).is_ok() {
-        debug!(
-          category = "CONFIG",
-          message = format!("Found the configuration file '{}'.", file_name)
-        );
-        return Ok(path.into());
+    if fs::metadata(&path).is_ok() {
+      debug!(
+        category = "CONFIG",
+        message = format!("Found the configuration file '{}'.", path_override)
+      );
+      return Ok(path.into());
+    }
+
+    Err(FastReleaseError {
+      message: "Couldn't find the configuration file.",
+      category: Some("CONFIG"),
+      error: None,
+    })
+  } else {
+    debug!(
+      category = "CONFIG",
+      message = "Trying to find a configuration file."
+    );
+
+    for file_name in CONFIG_FILE_NAMES {
+      for file_ext in CONFIG_FILE_EXTS {
+        let file_name: String = format!("{}.{}", file_name, file_ext);
+        let path: PathBuf = path.join(&file_name);
+
+        if fs::metadata(&path).is_ok() {
+          debug!(
+            category = "CONFIG",
+            message = format!("Found the configuration file '{}'.", file_name)
+          );
+          return Ok(path.into());
+        }
       }
     }
-  }
 
-  Err(FastReleaseError {
-    message: "Couldn't find a configuration file.",
-    category: Some("CONFIG"),
-    error: None,
-  })
+    Err(FastReleaseError {
+      message: "Couldn't find a configuration file.",
+      category: Some("CONFIG"),
+      error: None,
+    })
+  }
 }
 
 fn get_config(path: PathBuf) -> Result<RawConfig, FastReleaseError<'static>> {
@@ -171,20 +193,25 @@ fn convert_branches(config_branches: Vec<ConfigBranchEnum>) -> Vec<Branch> {
   converted_branches
 }
 
-fn transform_config(raw_config: RawConfig) -> Config {
+fn transform_config(raw_config: RawConfig, cli_params: CliParams) -> Config {
+  let dry_run: bool = cli_params.dry_run;
+
   Config {
     version: raw_config.version,
+    tag_format: raw_config.tag_format,
     modules: raw_config.modules,
     branches: convert_branches(raw_config.branches),
     project: raw_config.project,
     projects: raw_config.projects,
+    dry_run,
   }
 }
 
 pub fn get() -> Result<Config, FastReleaseError<'static>> {
-  let find_file: PathBuf = find_file()?;
+  let get_cli_params: CliParams = cli::get();
+  let find_file: PathBuf = find_file(get_cli_params.config_file_path.clone())?;
   let get_config: RawConfig = get_config(find_file)?;
-  let transform_config: Config = transform_config(get_config);
+  let transform_config: Config = transform_config(get_config, get_cli_params);
 
   Ok(transform_config)
 }
