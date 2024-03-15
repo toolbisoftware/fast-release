f8b9aeb516d1fed263b0bc04a97b40f75f7cf493
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-03-15 18:03:19 +0100

    Update git.rs

--- crates/env-ci/src/services/git.rs
+++ crates/env-ci/src/services/git.rs
@@ -1,2 +1,21 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
+
+use crate::{
+  builder::CiEnvBuilder,
+  util::{env_var_exists, get_branch, get_commit},
+  CiEnv, CiServices,
+};
+use std::collections::HashMap;
+
+pub fn get(env: &HashMap<String, String>) -> CiEnv {
+  let commit: Option<String> = get_commit();
+  let branch: Option<String> = get_branch();
+  let is_ci: bool = env_var_exists(env, "CI");
+
+  CiEnvBuilder::new("Git", CiServices::Git)
+    .commit(commit)
+    .branch(branch)
+    .is_ci(is_ci)
+    .get()
+}
