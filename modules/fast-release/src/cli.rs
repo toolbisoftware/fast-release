473ff2faf2488e72b5b4132a2eaaab10f1660f8d
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-03-15 16:16:56 +0100

    Update cli.rs

--- modules/fast-release/src/cli.rs
+++ modules/fast-release/src/cli.rs
@@ -1,56 +1,57 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
-// TODO Maybe use a propietary system in the future.
+// TODO Maybe use a custom CLI in the future
 
 use clap::{Arg, ArgAction, ArgMatches, Command};
 use tracing::warn;
 
 #[derive(Debug)]
 pub struct CliParams {
-  pub config_file_path: Option<String>,
   pub dry_run: bool,
+  pub config_file_path: Option<String>,
 }
 
 pub fn get() -> CliParams {
-  let mut cli_params: CliParams = CliParams {
-    config_file_path: None,
-    dry_run: false,
-  };
-  let clap: ArgMatches = Command::new("FastRelease CLI")
-    // .about("")
+  let clap: ArgMatches = Command::new("FastRelease CLI") /* .about("") */
     .arg(
-      Arg::new("dry")
+      Arg::new("dry_run")
         .long("dry")
         .short('d')
         .action(ArgAction::SetTrue)
-        .help("Disables publishing a new release for the current run"),
+        .help("Disables release publishing"),
     )
     .arg(
-      Arg::new("config")
+      Arg::new("config_file_path")
         .long("config")
         .short('c')
         .help("Sets the path to a custom configuration file"),
     )
     .get_matches();
 
-  if let Some(dry) = clap.get_one::<bool>("dry") {
-    let dry = dry.to_owned();
-    if dry {
+  let config_file_path: Option<String> = {
+    let value: Option<String> = clap
+      .get_one::<String>("config_file_path")
+      .map(|v| v.to_string());
+    if value.is_some() {
       warn!(
-        message = "Running in dry mode. A release won't be published.",
+        message = "Using a custom configuration file.",
         category = "CLI"
-      );
+      )
     }
-    cli_params.dry_run = dry;
-  }
-  if let Some(config) = clap.get_one::<String>("config") {
-    warn!(
-      message = "Using a custom set configuration file.",
-      category = "CLI"
-    );
-    cli_params.config_file_path = Some(config.to_owned())
-  }
+    value
+  };
 
-  cli_params
+  let dry_run: bool = {
+    let value: bool = clap.get_one::<bool>("dry_run").unwrap_or(&false).to_owned();
+    if value {
+      warn!(message = "Forcing a dry run.", category = "CLI")
+    }
+    value
+  };
+
+  CliParams {
+    config_file_path,
+    dry_run,
+  }
 }
