a471c72256d7043ca328e6bf195852ad244841b9
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-03-15 12:52:08 +0100

    Update constants.rs

--- modules/fast-release/src/constants.rs
+++ modules/fast-release/src/constants.rs
@@ -1,5 +1,5 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
-pub const CONFIG_FILE_NAMES: [&str; 2] = [".fast-release", "fast-release"];
-pub const CONFIG_FILE_EXTS: [&str; 2] = ["yml", "yaml"];
+pub const CONFIG_FILE_NAME: [&str; 2] = [".fast-release", "fast-release"];
+pub const CONFIG_FILE_EXT: [&str; 2] = ["yml", "yaml"];
