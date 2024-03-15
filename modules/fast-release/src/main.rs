4485725e80e59cc70f5e0f45c72ed697edd28604
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-03-15 16:22:04 +0100

    Update main.rs

--- modules/fast-release/src/main.rs
+++ modules/fast-release/src/main.rs
@@ -1,17 +1,46 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
-use log::info;
+// TODO Maybe create a config file if the config file doesn't exist or it's empty
+// Could be an argument on the CLI too
 
+mod cli;
+mod config;
 mod constants;
-mod util;
+mod error;
 
-fn setup_logger() {
-  util::logger::init(log::LevelFilter::Info);
+use crate::{cli::CliParams, config::Config};
+use commonlib::{logger::LoggerBuilder, Logger};
+use error::{soft_panic, FastReleaseError, FastReleaseErrorBuilder};
+use tracing::info;
+
+fn init_logger() -> Result<(), FastReleaseError> {
+  match Logger::init(LoggerBuilder {
+    level: Some(tracing::Level::DEBUG),
+    file_logging: None,
+  }) {
+    Ok(_) => Ok(()),
+    Err(error) => Err(
+      FastReleaseErrorBuilder::new("Failed to initialize the logger.")
+        .error(error)
+        .get(),
+    ),
+  }
+}
+
+fn run() -> Result<(), FastReleaseError> {
+  init_logger()?;
+  info!("Running FastRelease v{}.", env!("CARGO_PKG_VERSION"));
+
+  let cli_params: CliParams = cli::get();
+  let config: Config = config::get(&cli_params)?;
+
+  Ok(())
 }
 
 fn main() {
-  setup_logger();
-
-  info!("Running fast-release {}.", constants::VERSION);
+  match run() {
+    Ok(_) => {}
+    Err(error) => soft_panic(error),
+  }
 }
