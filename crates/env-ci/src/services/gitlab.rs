2f322f3f3877a47d73143f1943c2acad360974cd
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-03-25 17:53:42 +0100

    Update gitlab.rs

--- crates/env-ci/src/services/gitlab.rs
+++ crates/env-ci/src/services/gitlab.rs
@@ -15,45 +15,31 @@ pub fn detect(env: &HashMap<String, String>) -> bool {
 pub fn get(env: &HashMap<String, String>) -> CiEnv {
   let ci_project_url: Option<String> = get_env_var(env, "CI_PROJECT_URL");
 
-  let slug: Option<String> = get_env_var(env, "CI_PROJECT_PATH");
-  let root: Option<String> = get_env_var(env, "CI_PROJECT_DIR");
-  let commit: Option<String> = get_env_var(env, "CI_COMMIT_SHA");
-  let tag: Option<String> = get_env_var(env, "CI_COMMIT_TAG");
   let pull_request: Option<String> = get_env_var(env, "CI_MERGE_REQUEST_ID");
-  let branch: Option<String> = if pull_request.is_some() {
-    get_env_var(env, "CI_MERGE_REQUEST_TARGET_BRANCH_NAME")
-  } else {
-    get_env_var(env, "CI_COMMIT_REF_NAME")
-  };
-  let pull_request_branch: Option<String> = get_env_var(env, "CI_MERGE_REQUEST_SOURCE_BRANCH_NAME");
+  let branch: Option<String> = pull_request
+    .as_ref()
+    .and_then(|_| get_env_var(env, "CI_MERGE_REQUEST_TARGET_BRANCH_NAME"))
+    .or_else(|| get_env_var(env, "CI_COMMIT_REF_NAME"));
   let job: Option<String> = get_env_var(env, "CI_JOB_ID");
-  let job_url: Option<String> = {
-    if let Some(job) = &job {
-      if let Some(ci_project_url) = &ci_project_url {
-        Some(format!("{}/-/jobs/{}", ci_project_url, job));
-      }
-    }
-    None
-  };
+  let job_url: Option<String> = job.as_ref().and_then(|j| {
+    ci_project_url
+      .as_ref()
+      .map(|cpu: &String| format!("{}/-/jobs/{}", cpu, j))
+  });
   let build: Option<String> = get_env_var(env, "CI_PIPELINE_ID");
-  let build_url: Option<String> = {
-    if let Some(build) = &build {
-      if let Some(ci_project_url) = ci_project_url {
-        Some(format!("{}/pipelines/{}", ci_project_url, build));
-      }
-    }
-    None
-  };
+  let build_url: Option<String> = build
+    .as_ref()
+    .and_then(|b: &String| ci_project_url.map(|cpu: String| format!("{}/pipelines/{}", cpu, b)));
   let is_pull_request: bool = pull_request.is_some();
 
   CiEnvBuilder::new("GitLab CI/CD", CiServices::GitLab)
-    .slug(slug)
-    .root(root)
-    .commit(commit)
-    .tag(tag)
+    .slug(get_env_var(env, "CI_PROJECT_PATH"))
+    .root(get_env_var(env, "CI_PROJECT_DIR"))
+    .commit(get_env_var(env, "CI_COMMIT_SHA"))
+    .tag(get_env_var(env, "CI_COMMIT_TAG"))
     .branch(branch)
     .pull_request(pull_request)
-    .pull_request_branch(pull_request_branch)
+    .pull_request_branch(get_env_var(env, "CI_MERGE_REQUEST_SOURCE_BRANCH_NAME"))
     .job(job)
     .job_url(job_url)
     .build(build)
